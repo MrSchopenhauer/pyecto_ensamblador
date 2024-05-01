2c899c21841a26b72940376b82fe25d235d5cd98
MrSchopenhauer <jose.uniriva15@gmail.com>
2024-05-01 01:35:23 -0600

    Se termino la version de prueba del test.py primera prueba

--- fase_uno/test.py
+++ fase_uno/test.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify  
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/procesar', methods=['POST'])
+def procesar():
+	if 'file' not in request.files:
+		return jsonify({'error': 'No file part'})
+
+	file = request.files['file']
+
+	if file.filename == '':
+		return jsonify({'error':'No selected file'})
+
+	contenido = file.read().decode('utf-8')	
+	
+	return jsonify({'content': contenido})
+		
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
